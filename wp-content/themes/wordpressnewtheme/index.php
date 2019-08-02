a76fb754c3fa5d4c156dc306bc1811d8a35e2274
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-02 22:37:06 +0200

    index.php inside themes folder wordpressnewtheme was updated

--- wp-content/themes/wordpressnewtheme/index.php
+++ wp-content/themes/wordpressnewtheme/index.php
@@ -1,1 +1,32 @@
-This is a my New WordPress Theme
+<?php
+
+//echo 2 + 2;
+
+
+//function myFirstFunction() {
+//    echo "<p>Hello U There - This my first funtion </p>";
+//}
+//
+//myFirstFunction();
+//
+//myFirstFunction();
+//
+//myFirstFunction();
+//
+//myFirstFunction();
+
+
+function greetings($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is <font size='4' color='$color'>$color</font></p>";
+}
+
+greetings("Silvia","darkred");
+
+greetings("David","purple");
+
+
+?>
+
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
