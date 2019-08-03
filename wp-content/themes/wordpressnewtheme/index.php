19d2b2237192ca4e82701fc7741311581f9a0164
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-03 12:19:22 +0200

    index.php inside themes folder wordpressnewtheme was updated - retrieving blogpost from WordPress

--- wp-content/themes/wordpressnewtheme/index.php
+++ wp-content/themes/wordpressnewtheme/index.php
@@ -1,55 +1,11 @@
 <?php
 
-//echo 2 + 2;
 
+while( have_posts() ) {
+    the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <?php the_content(); ?>
+    <hr>
+<?php }
 
-//function myFirstFunction() {
-//    echo "<p>Hello U There - This my first funtion </p>";
-//}
-//
-//myFirstFunction();
-//
-//myFirstFunction();
-//
-//myFirstFunction();
-//
-//myFirstFunction();
-
-
-//function greetings($name, $color) {
-//    echo "<p>Hi, my name is $name and my favorite color is <font size='4' color='$color'>$color</font></p>";
-//}
-//
-//greetings("Silvia","darkred");
-//
-//greetings("David","purple");
-
-$names = array('Brad', 'Jennifer', 'Patrick', 'David', 'Kevin');
-
-//$count = 1;
-//while($count <= 100) {
-//    echo "<li>$count</li>";
-//    $count = $count +1;
-//}
-//$count = 0;
-//while($count < $names.length) {
-//    echo "<li>Hi, my name is $names[$count];</li>";
-//        $count++;
-//}
-
-foreach ($names as $index)
-{
-    echo "$index <br>";
-}
 ?>
-
-<!--
-<p>Hi, my name is <?php #echo $names[0]; ?> </p>
-<p>Hi, my name is <?php #echo $names[1]; ?> </p>
-<p>Hi, my name is <?php #echo $names[2]; ?> </p>
-<p>Hi, my name is <?php #echo $names[3]; ?> </p>
--->
-
-
-
-
