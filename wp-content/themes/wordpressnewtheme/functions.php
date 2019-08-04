606989266c2cd95b36090f0fb0c0eda4511f6966
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-04 14:31:42 +0200

    functions.php and header.php and footer.php and style.css and index.php files inside custom theme folder wordpressnewtheme was updated

--- wp-content/themes/wordpressnewtheme/functions.php
+++ wp-content/themes/wordpressnewtheme/functions.php
@@ -2,8 +2,12 @@
 
 
 function custom_theme_files() {
-    wp_enqueue_style('main_style', get_stylesheet_uri());
     
+    wp_enqueue_script('main-js',get_theme_file_uri('js/scripts-bundled.js'),'NULL', '1.0', true);
+    wp_enqueue_style('main_style', get_stylesheet_uri());
+    #wp_enqueue_style('main_style', get_stylesheet_uri(), NULL, microtime()); NULL MEANS thaht this stylesheet has no dependency, microstime is php function that generates an automatic string for the stylesheet version
+    wp_enqueue_style('font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
 
