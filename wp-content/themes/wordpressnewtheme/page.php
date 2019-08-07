0e1f32dc7946056bd974fbcf9888c0f330dfd0c0
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-07 11:23:37 +0200

    page.php file inside custom theme folder wordpressnewtheme was updated

--- wp-content/themes/wordpressnewtheme/page.php
+++ wp-content/themes/wordpressnewtheme/page.php
@@ -16,11 +16,14 @@ while( have_posts() ) {
   </div>
 
   <div class="container container--narrow page-section">
-
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
-    </div>
     
+    <?php 
+        if (wp_get_post_parent_id(get_the_ID())) { ?>
+             <div class="metabox metabox--position-up metabox--with-home-link"><p><a class="metabox__blog-home-link" href="<?php echo get_permalink(wp_get_post_parent_id(get_the_ID()));  ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(wp_get_post_parent_id(get_the_ID())); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p></div>
+            
+    <?php }
+    ?>
+   
 <!--
         <div class="page-links">
           <h2 class="page-links__title"><a href="#">About Us</a></h2>
