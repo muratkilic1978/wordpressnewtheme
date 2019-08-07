e9144ee64ece3f775d613fc2b607097fe0ce4e53
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-07 17:43:16 +0200

    header.php file inside custom theme folder wordpressnewtheme was updated

--- wp-content/themes/wordpressnewtheme/header.php
+++ wp-content/themes/wordpressnewtheme/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="<?php bloginfo('charset'); ?>">
     <?php wp_head(); ?>
     
 </head>
-<body>
+<body <?php body_class(); ?>>
     
 <header class="site-header">
     <div class="container">
