66bdf122c259c491100bdd582c5cfb23e32d1a4e
muratkilic1978 <muratkilic1978@gmail.com>
2019-08-09 18:49:24 +0200

    footer.php and header.php and functions.pho was updated

--- wp-content/themes/wordpressnewtheme/functions.php
+++ wp-content/themes/wordpressnewtheme/functions.php
@@ -14,6 +14,9 @@ add_action('wp_enqueue_scripts', 'custom_theme_files');
 
 function custom_theme_features() {
     
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
