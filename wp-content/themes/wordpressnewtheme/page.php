68ac92a4b58c7b78607e35f090b4b5ae824ae2f3
muratkilic1978 <muratkilic1978@gmail.com>
2019-12-21 12:04:33 +0100

    hamilton theme was added and front-page.php - page.php - index.php and finally single.php were modified

--- wp-content/themes/wordpressnewtheme/page.php
+++ wp-content/themes/wordpressnewtheme/page.php
@@ -6,14 +6,14 @@ while( have_posts() ) {
     the_post(); ?>
 
       <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>Dont Forget to replace me later</p>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p>Dont Forget to replace me later</p>
+            </div>
+        </div>  
       </div>
-    </div>  
-  </div>
 
   <div class="container container--narrow page-section">
     
